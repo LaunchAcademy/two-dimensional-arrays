96958a3ec14ad88eebc71ee42d4f30c5eb8365bb
Stephen Fang <safang99@gmail.com>
2022-01-25 20:04:22 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const studentOneGrades = [90, 86, 100, 85]
+const studentTwoGrades = [65, 81, 96, 100]
+const studentThreeGrades = [79, 94, 85, 95]
+
+const studentGrades = [studentOneGrades, studentTwoGrades, studentThreeGrades]
+
+// What did student three get on the first exam?
+
+// What did student two get on the third exam?
+
+// What did student one get on the last exam?
+
+// What is student 2's average?
+
+// Create a new array of all third exam grades
+
+// What was the average grade on exam 1?
+
+// What was the worst score on exam 2?
+
+// What is the aggregate average test score?
